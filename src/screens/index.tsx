39dc6e87561a5520c6732443f46eb70fdd253842
Leonardo Silva <leonardosilvaif@hotmail.com>
2021-07-23 10:45:56 -0300

    Creating src folder and adding Home screen

---
+++ src/screens/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Esta é a tela HOME!
+            </Text>
+        </View>
+    );
+}
