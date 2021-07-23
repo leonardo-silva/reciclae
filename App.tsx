fbefbe91143fb7e9c41f57080815ecd1eb9903f3
Leonardo Silva <leonardosilvaif@hotmail.com>
2021-07-23 13:22:54 -0300

    Creating Home folder

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import { Home } from './src/screens';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
